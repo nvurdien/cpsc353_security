e91270afb6066d278fb71607c72954c7263d01ab
christophergrant <christophergrant@openmailbox.org>
2017-06-05 10:58:12 -0700

    for navie lul

--- as1/rbac.py
+++ as1/rbac.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     with open(sys.argv[1]) as g, open(sys.argv[2]) as r, open(sys.argv[3]) as a:
+        g = parse_groups(g)
         r = parse_acl(r)
 
         pass
@@ -13,6 +14,11 @@ def main():
 def parse_acl(acl):
     pass
 
+def parse_groups(g):
+    for l in g:
+        
+
+    return dict([v,k] for k,v in g.iteritems())
 
 def usage(script):
     print("USAGE: %s groups resources actions" % script)
