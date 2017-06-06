a0dfac5333651d28157af029aee6321cf8cd2c7e
nvurdien <nvurdien@gmail.com>
2017-06-06 11:51:41 -0700

    I think its done wow

--- as1/rbac.py
+++ as1/rbac.py
@@ -10,10 +10,22 @@ def main():
 
     with open(sys.argv[2],'rb') as acl:
         acl = parse_acl(acl)
-        print(acl)
 
     with open(sys.argv[3],'rb') as a:
+        for l in a:
+            user, action, direc = l.split(" ")
+            dir_access = acl[direc.strip()]
+            allow = False
+            for x in g[user.strip()]:
+                if x in dir_access:
+                    if action in dir_access[x]:
+                        allow = True
+            if allow:
+                print "ALLOW %s %s %s" %(user, action, direc)
+            else:
+                print "DENY %s %s %s" %(user, action, direc)
         pass
+
 
 def parse_acl(acl):
     d = collections.defaultdict(lambda: collections.defaultdict(list))
@@ -32,10 +44,10 @@ def parse_groups(g):
     d = collections.defaultdict(list)
     for l in g:
         l = l.strip()
-        if any(l): 
+        if any(l):
             group, members = clean_line(l)
             d[group] = members
-    inv_d = collections.defaultdict(list) 
+    inv_d = collections.defaultdict(list)
     for key, values in d.iteritems():
         for value in values:
             inv_d[value].append(key)
