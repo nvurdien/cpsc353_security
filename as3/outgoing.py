02c0617f9cee4861b281e7c2a536d58510c2d562
christophergrant <christophergrant@openmailbox.org>
2017-06-17 02:02:44 -0700

    added outgoing, still missing async hostname functionality

--- as3/outgoing.py
+++ as3/outgoing.py
@@ -3,13 +3,30 @@
 sp00ky skelet0n
 """
 import socket
+from scapy.all import *
+
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+s = set()
+this_ip = get_ip_address()
 
 def main():
-    pass
+    print this_ip
+    sniff(prn=print_new_ip,store=0)
 
 def resolve_host(ip):
     return socket.gethostbyaddr(ip)[0]
 
+def print_new_ip(pkt):
+    if IP in pkt:
+        src_ip = pkt[IP].src
+        dest_ip= pkt[IP].dst
+        if src_ip == this_ip and dest_ip not in s:
+            s.add(dest_ip)
+            return dest_ip
 
 if __name__=="__main__":
     main()
