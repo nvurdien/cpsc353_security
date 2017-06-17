936424ed5d066a27565a175cf2f05f13ca7344d7
christophergrant <christophergrant@openmailbox.org>
2017-06-17 00:39:05 -0700

    start as3

---
+++ as3/basicauth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+"""
+sp00ky skelet0n
+"""
+
+def main():
+        
+
+
+if __name__=="__main__":
+    main()
---
+++ as3/outgoing.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+"""
+sp00ky skelet0n
+"""
+import socket
+
+def main():
+    pass
+
+def resolve_host(ip):
+    return socket.gethostbyaddr(ip)[0]
+
+
+if __name__=="__main__":
+    main()
